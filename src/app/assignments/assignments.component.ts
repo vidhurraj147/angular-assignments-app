bb8c575b13cec819eee3642344e0e1c7ce12fb35
vidhurraj147 <vidhurraj147@gmail.com>
2019-01-20 22:42:21 -0500

    Created component _ String interpolation

--- src/app/assignments/assignments.component.ts
+++ src/app/assignments/assignments.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AssignmentsComponent implements OnInit {
 
+  title = "My Assignments Component";
+
   constructor() { }
 
   ngOnInit() {
